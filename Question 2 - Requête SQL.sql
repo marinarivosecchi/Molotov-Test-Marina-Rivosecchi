6dc807fad3ce2288486eb0f3ff375cb9e3de9982
Marina Rivosecchi <marinarivosecchi@gmail.com>
2024-04-09 08:50:00 +0200

    Update Question 2 - Requête SQL.sql

--- Question 2 - Requête SQL.sql
+++ Question 2 - Requête SQL.sql
@@ -1,32 +1,34 @@
--- Sélection des utilisateurs âgés de plus de 30 ans, inscrits sur Molotov depuis moins de 12 mois,
--- ayant visionné plus de 2h de film sur mobile en Mai 2023
+-- Selection of users aged over 30, registered on Molotov for less than 12 months, having watched 
+-- more than 2 hours of film on mobile in May 2023
 WITH UserCriteria AS (
-    SELECT U.USER_ID
+    SELECT U.ID as USER_ID
     FROM User U
-    WHERE EXTRACT(YEAR FROM CURRENT_DATE) - EXTRACT(YEAR FROM U.BIRTHDAY) > 30 -- Plus de 30 ans
-    AND TIMESTAMPDIFF(MONTH, U.CREATED_AT, CURRENT_DATE) < 12 -- Inscrit depuis moins de 12 mois
+    WHERE EXTRACT(YEAR FROM CURRENT_DATE) - EXTRACT(YEAR FROM U.BIRTHDAY) > 30 -- More than 30 y/o
+    AND DATE_DIFF( CREATED_AT, CURRENT_DATE, MONTH) < 12 -- Registered for less than 12 months
     AND EXISTS (
         SELECT 1
         FROM Watch W
-        WHERE W.USER_ID = U.USER_ID
-        AND MONTH(W.DATE_DAY) = 5 -- Visionné en Mai 2023
-        AND W.DEVICE_TYPE = 'Mobile' -- Sur mobile
-        AND W.DURATION > 7200 -- Plus de 2h de visionnage
-    )
-    );
-
--- Sélection des utilisateurs abonnés à Molotov Extra
+        WHERE W.USER_ID = U.ID
+        AND EXTRACT(YEAR FROM W.DATE_DAY) = 2023 -- Viewed in 2023
+        AND EXTRACT(MONTH FROM W.DATE_DAY) = 5 -- Viewed in May
+        AND W.DEVICE_TYPE = 'phone' -- With Mobile devices
+        AND W.PROGRAM_KIND = ‘Films’ -- Watched films
+        GROUP BY W.USER_ID
+        HAVING SUM(W.DURATION) > 7200 -- More than 2h of total viewing time
+    )),
+-- Selection of users that meet criteria and are currently subscribed to Molotov Extra
 MolotovExtraSubscribers AS (
     SELECT DISTINCT S.USER_ID 
     FROM Subscription S
-    WHERE PRODUCT_NAME = 'Molotov Extra'
-    AND (SUBSCRIPTION_END_DATE IS NULL OR SUBSCRIPTION_END_DATE > CURRENT_DATE) -
+    INNER JOIN UserCriteria UC -- From users that meet criteria, only those that are subscribed
+    ON UC.USER_ID = S.USER_ID
+    WHERE PRODUCT_NAME = 'Molotov Extra' -- Subscribed to Molotov Extra
+    AND (SUBSCRIPTION_END_DATE IS NULL OR SUBSCRIPTION_END_DATE > CURRENT_DATE) -- Current subscription 
 ),
-
--- Calcul de la part d'utilisateurs abonnés à Molotov Extra parmi les utilisateurs répondant aux critères
+-- Calculation of the share of users subscribed to Molotov Extra among users meeting the above criteria
 SELECT 
-    COUNT(DISTINCT UC.USER_ID) AS Nombre_Utilisateurs,
-    COUNT(DISTINCT ES.USER_ID) AS Utilisateurs_Abonnés_Extra,
-    COUNT(DISTINCT ES.USER_ID) / COUNT(DISTINCT UC.USER_ID) AS Part_Abonnés_Extra
+    COUNT(DISTINCT UC.USER_ID) AS Total_Users, -- Count of unique users that meet criteria
+    COUNT(DISTINCT ES.USER_ID) AS Molotov_Extra_Total_Users, -- Count of unique users subscribed to Molotov Extra
+    COUNT(DISTINCT ES.USER_ID) / COUNT(DISTINCT UC.USER_ID) AS Share_of_Molotov_Extra_Users
 FROM UserCriteria UC
 LEFT JOIN MolotovExtraSubscribers ES ON UC.USER_ID = ES.USER_ID;
